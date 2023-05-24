3a110b6f339268a1755fe86a1906bfd1008ed005
Rick189 <87337111+Rick189@users.noreply.github.com>
2023-05-25 00:56:06 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,53 +15,68 @@ plt.title("(a)")
 #вычисление НСКО
 def error(img):
     global image
-    #деление на два у img в следствии того, что у него амплитуда в 2 раза больше
-    return np.sqrt(np.sum(np.abs((np.abs(img))/2-image/1)**2)/np.sum(((image/1)**2)))
-    #return np.linalg.norm(np.abs(img)-image,ord=2)/np.linalg.norm(image,ord=2)
+    s=image/np.max(image)
+    q=np.sqrt(img*np.conj(img))/np.max(np.sqrt(img*np.conj(img)))
+    return np.sqrt((np.sum(np.abs(s-q)**2))/np.sum(s**2))   #return np.linalg.norm(np.abs(img)-image,ord=2)/np.linalg.norm(image,ord=2)
 
+n=[]
+ermas=[]
+forplot=np.zeros(1000)
+d=np.zeros(1000)
 
-#загружаем случайную основу и целевую амплитуду    
-source=np.ones_like(image)*np.exp(1j*np.pi*2*(np.random.rand(image.shape[0],image.shape[1]))-0.5)
-target=np.abs(image)
-
-#начинаем алгоритм Гречберга-Секстона
-i=0
-
-img=source
-img=np.fft.fft2(img)
-
-#нужно для работы цикла, чтобы он не отключался сразу
-err=np.inf
-#массивы для графика
-x=[]
-y=[]
-
-while(np.abs(err-error(img))>=0.00001):
-    err=error(img)
-    if(i%10==0):
-        print('Iteration: '+str(i)+' Error: '+str(err))
-    #считаем график
-    x.append(i)
-    y.append(err)
-    #plt.figure()
-    #plt.imshow(np.abs(img),'gray')
-    #plt.title(i)
-    #устанавливаем целевую амплитуду
-    img=target*np.exp(1j*np.angle(img))
-    #обратное преобразование
-    img=np.fft.ifft2(img)
-    #извлекаем фазу и устанавливаем единичную амплитуду
-    img=np.exp(1j*np.angle(img))
-    #переход к изображению
+for j in range(5):
+    #загружаем случайную основу и целевую амплитуду    
+    source=np.ones_like(image)*np.exp(1j*np.pi*2*(np.random.rand(image.shape[0],image.shape[1]))-0.5)
+    target=np.abs(image)
+    
+    #начинаем алгоритм Гречберга-Секстона
+    i=0
+    
+    img=source
     img=np.fft.fft2(img)
-    i+=1
-print(i,error(img))
+    
+    #нужно для работы цикла, чтобы он не отключался сразу
+    err=np.inf
+    #массивы для графика
+    x=[]
+    y=[]
+    
+    while(np.abs(err-error(img))>=0.00001):
+        err=error(img)
+        if(i%10==0):
+            print('Iteration: '+str(i)+' Error: '+str(err))
+        #считаем график
+        x.append(i)
+        y.append(err)
+        #plt.figure()
+        #plt.imshow(np.abs(img),'gray')
+        #plt.title(i)
+        #устанавливаем целевую амплитуду
+        img=target*np.exp(1j*np.angle(img))
+        #обратное преобразование
+        img=np.fft.ifft2(img)
+        #извлекаем фазу и устанавливаем единичную амплитуду
+        img=np.exp(1j*np.angle(img))
+        #переход к изображению
+        img=np.fft.fft2(img)
+        i+=1
+    print(i,error(img))
+    n.append(i)
+    ermas.append(error(img))
+    d+=np.ones_like(np.concatenate((y,np.zeros(len(forplot)-len(y)))))
+    y=np.concatenate((y,np.zeros(len(forplot)-len(y))))
+    forplot+=np.concatenate((y,np.zeros(len(forplot)-len(y))))
 #plt.figure()
 plt.subplot(132)
 plt.imshow(np.abs(img)/2,'gray')
 plt.title("(b)")
 plt.figure(133)
-plt.plot(x,y,mec='b')
+nsr=np.mean(n)
+nerr=np.mean(ermas)
+x=range(int(nsr))
+y=forplot/d
+plt.plot(x,y[:int(nsr)],mec='b')
 plt.xlabel("i, число итераций")
 plt.ylabel("E, ошибка формирования изображения")
 plt.title("(c)")
+print(nsr,' ',np.std(n),' ',nerr,' ',np.std(ermas))
